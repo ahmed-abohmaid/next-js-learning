3af261031dbb5723586a5391e114064be86331c2
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-03-18 06:53:15 +0200

    change project description for seo

--- Projects/blog/app/layout.tsx
+++ Projects/blog/app/layout.tsx
@@ -1,11 +1,12 @@
-import MyProfilePic from './components/MyProfilePic';
-import Navbar from './components/Navbar';
-import './globals.css';
-import type { Metadata } from 'next';
+import MyProfilePic from "./components/MyProfilePic";
+import Navbar from "./components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Ahmed's Blog",
-  description: 'Created by Ahmed Ramadan',
+  title: "Ahmed's Test Blog",
+  description:
+    "Created by Ahmed Ramadan, this is a Next JS project which I made to learn about SSG. It's a test blog",
 };
 
 export default function RootLayout({
